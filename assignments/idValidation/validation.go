c83bbc03a5e3ca14d4517b7c0ca91f416b84b2dd
sina neysi <s1n4.ne1331@gmail.com>
2025-01-30 20:20:13 +0330

    ADD: add luhn

--- assignments/idValidation/validation.go
+++ assignments/idValidation/validation.go
@@ -3,18 +3,61 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
-func check(social string) {
+func validationCheck(social string) int {
+	var sum int
+	var numRegex = regexp.MustCompile(`^[0-9 ]+$`)
 
-}
-func main() {
-	name := "this is sina"
-	var socialNumber string = "3381381923"
-	var numRegex = regexp.MustCompile(`^[0-9]+$`)
-	if numRegex.MatchString(socialNumber) {
+	if numRegex.MatchString(social) {
+		social = strings.ReplaceAll(social, " ", "") // Remove spaces
+		idArray := convertStringToArray(social)      // Get slice from function
 
+		// Step 1: Process every second digit
+		for i := len(idArray) - 2; i >= 0; i = i - 2 { // Fixed loop condition
+			convertstrtoint, err := strconv.Atoi(idArray[i])
+			if err != nil {
+				panic(err)
+			}
+			doubledNum := convertstrtoint * 2
+
+			if doubledNum > 9 {
+				idArray[i] = strconv.Itoa(doubledNum - 9)
+			} else {
+				idArray[i] = strconv.Itoa(doubledNum)
+			}
+		}
+
+		// Step 2: Sum up all digits
+		for i := 0; i < len(idArray); i++ {
+			convertedStr, err := strconv.Atoi(idArray[i])
+			if err != nil {
+				panic(err)
+			}
+			sum += convertedStr
+		}
 	}
-	fmt.Println(strings.ReplaceAll(name, " ", ""))
+	return sum
+}
+
+func convertStringToArray(str string) []string {
+	array := make([]string, len(str)) // Correct slice allocation
+	for index, value := range str {
+		array[index] = string(value) // Proper assignment
+	}
+	return array
+}
+
+func main() {
+	var socialNumber string = "4539 3195 0343 6467"
+	validCheck := false
+	result := validationCheck(socialNumber) // Test the function
+	if result%10 == 0 {
+		validCheck = true
+	} else {
+		validCheck = false
+	}
+	fmt.Println(validCheck)
 }
